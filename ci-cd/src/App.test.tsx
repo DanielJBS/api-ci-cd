56e8b95bc5ed3bde787bd9cd53962da33d5a5e2b
DanielJBS <156136330+DanielJBS@users.noreply.github.com>
2025-05-05 14:31:34 -0700

    Add files via upload

---
+++ ci-cd/src/App.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from "./App";
+
+describe("App", () => {
+  it("renders Vite and React logos", () => {
+    render(<App />);
+    const viteLogo = screen.getByAltText("Vite logo");
+    const reactLogo = screen.getByAltText("React logo");
+
+    expect(viteLogo).toBeInTheDocument();
+    expect(reactLogo).toBeInTheDocument();
+  });
+
+  it("renders initial count", () => {
+    render(<App />);
+    expect(screen.getByText("count is 0")).toBeInTheDocument();
+  });
+
+  it("increments count on click", () => {
+    render(<App />);
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(screen.getByText("count is 1")).toBeInTheDocument();
+  });
+
+  //it("renders custom message", () => {
+  //  render(<App />);
+  //  expect(screen.getByText(/dshyjGADSKUGVSA/i)).toBeInTheDocument();
+  //});
+});
